5ea252d4fd744821af60e8853daa9cb20e2a4c9a
Nikita Sigal <parallax.nikitasigal@gmail.com>
2023-01-11 11:48:25 +0300

    Initial Commit

---
+++ PetLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetLand
+//
+//  Created by Никита Сигал on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
