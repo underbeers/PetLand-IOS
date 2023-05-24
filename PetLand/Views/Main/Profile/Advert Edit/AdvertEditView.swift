ca8265a82c6ae94620b35a3c806042f9e0fd0870
Nikita Sigal <nikitasigal@icloud.com>
2023-05-24 14:24:53 +0300

    [PL-220] Support for adding and editing location of adverts

--- PetLand/Views/Main/Profile/Advert Edit/AdvertEditView.swift
+++ PetLand/Views/Main/Profile/Advert Edit/AdvertEditView.swift
@@ -21,12 +21,16 @@ struct AdvertEditView: View {
     @State var descriptionIsValid: Bool = false
     @State var priceIsValid: Bool = false
     @State var contactsAreValid: Bool = false
+    @State var cityIsValid: Bool = false
+    @State var districtIsValid: Bool = false
     
     var canCommit: Bool {
         (initialAdvert != nil || cardIsValid)
             && descriptionIsValid
             && priceIsValid
             && contactsAreValid
+            && cityIsValid
+            && districtIsValid
     }
     
     var body: some View {
@@ -50,13 +54,30 @@ struct AdvertEditView: View {
                     ContactsPicker(useMessenger: $model.advert.chat, phone: $model.advert.phone)
                 }
                 
+                CustomWrapper(title: "Город", isValid: $cityIsValid) {
+                    CustomDropdown(options: model.cities.map { $0.name }, selection: $model.advert.city)
+                }
+                .onChange(of: model.advert.city) { _ in
+                    model.fetchDistricts()
+                }
+                .onChange(of: model.districts) { _ in
+                    if !model.districts.contains(where: { $0.name == model.advert.district }) {
+                        model.advert.district = ""
+                    }
+                }
+                
+                CustomWrapper(title: "Район", isValid: $districtIsValid) {
+                    CustomDropdown(options: model.districts.map { $0.name }, selection: $model.advert.district)
+                }
+                .disabled(!cityIsValid)
+                
                 Button(initialAdvert == nil ? "Опубликовать объявление" : "Сохранить изменения") {
                     model.commitAdvert(isNew: initialAdvert == nil) {
                         dismiss()
                     }
                 }
-                    .buttonStyle(CustomButton(.primary, isEnabled: canCommit))
-                    .disabled(!canCommit)
+                .buttonStyle(CustomButton(.primary, isEnabled: canCommit))
+                .disabled(!canCommit)
             }
             .padding(16)
             .padding(.bottom, 32)
@@ -68,6 +89,7 @@ struct AdvertEditView: View {
             if let initialAdvert {
                 model.advert = .init(initialAdvert)
             }
+            model.fetchCities()
         }
         .alert("Что-то пошло не так...", isPresented: $model.presentingAlert) {
             Text(model.alertMessage)
--- PetLand/Views/Main/Profile/Advert Edit/AdvertEditViewModel.swift
+++ PetLand/Views/Main/Profile/Advert Edit/AdvertEditViewModel.swift
@@ -11,15 +11,18 @@ import PhoneNumberKit
 extension AdvertEditView {
     @MainActor final class AdvertEditViewModel: ObservableObject {
         private let advertService: AdvertServiceProtocol = AdvertService.shared
+        private let locationService: LocationServiceProtocol = LocationService.shared
 
         @Published var advert: AdvertEdit = .init()
+        
+        @Published var cities: [City] = []
+        @Published var districts: [District] = []
 
         @Published var alertMessage: String = ""
         @Published var presentingAlert: Bool = false
 
         func commitAdvert(isNew: Bool, _ completion: @escaping () -> () = {}) {
-            advert.cityID = 1
-            advert.districtID = 1
+            restoreDistrictID()
             advertService.commitAdvert(advert, isNew: isNew) { [weak self] error in
                 if let error {
                     switch error {
@@ -34,5 +37,56 @@ extension AdvertEditView {
                 }
             }
         }
+        
+        func restoreCityID() {
+            advert.cityID = cities.first(where: { $0.name == advert.city })?.id ?? advert.cityID
+        }
+        
+        func restoreDistrictID() {
+            advert.districtID = districts.first(where: { $0.name == advert.district })?.id ?? advert.districtID
+        }
+        
+        func fetchCities() {
+            locationService.getCities { [weak self] result in
+                switch result {
+                    case .success(let cities):
+                        self?.cities = cities
+                        self?.fetchDistricts()
+                    case .failure(let error):
+                        switch error {
+                            case APIError.serverDown:
+                                self?.alertMessage = "Проблема с доступом к серверу"
+                            default:
+                                self?.alertMessage = error.localizedDescription
+                        }
+                        self?.presentingAlert = true
+                }
+            }
+        }
+        
+        func fetchDistricts() {
+            restoreCityID()
+            guard advert.cityID != 0 else {
+                districts = []
+                return
+            }
+            
+            locationService.getDistricts(cityID: advert.cityID) { [weak self] result in
+                switch result {
+                    case .success(let districts):
+                        self?.districts = districts
+                    case .failure(let error):
+                        switch error {
+                            case APIError.serverDown:
+                                self?.alertMessage = "Проблема с доступом к серверу"
+                            default:
+                                self?.alertMessage = error.localizedDescription
+                        }
+                        self?.presentingAlert = true
+                }
+            }
+            
+        }
+        
     }
 }
